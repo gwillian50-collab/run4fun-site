d50c943dc099e90ffe384e1649ce9cf81121acd9
Gustavo <gwillian50@gmail.com>
2026-06-25 17:18:29 -0300

    fix: URL real do Instagram (run4fun_rc)

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const INSTAGRAM_URL = "https://instagram.com/SEU_USUARIO_AQUI"; // TODO: Gustavo substituir
+export const INSTAGRAM_URL = "https://www.instagram.com/run4fun_rc/";
 export const APP_URL = "https://run4fun-coach.vercel.app"; // TODO: confirmar domínio real do app
