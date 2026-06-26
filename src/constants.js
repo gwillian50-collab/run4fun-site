fef39a9fd8aec2348f52b735b748dd4c2d4b9d3c
Gustavo <gwillian50@gmail.com>
2026-06-26 14:49:04 -0300

    fix: atualiza APP_URL para o domínio customizado coach.run4fun.app.br

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 export const INSTAGRAM_URL = "https://www.instagram.com/run4fun_rc/";
-export const APP_URL = "https://run4fun-coach.vercel.app"; // TODO: confirmar domínio real do app
+export const APP_URL = "https://coach.run4fun.app.br";
